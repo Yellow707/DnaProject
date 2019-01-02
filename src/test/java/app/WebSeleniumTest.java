36311e2a4a702a39b75010c9a382a108b7615c16
Igor Petrov <fox0234@yandex.ru>
2019-01-02 18:43:48 +0300

    WebSeleniumTest turn off

--- src/test/java/app/WebSeleniumTest.java
+++ src/test/java/app/WebSeleniumTest.java
@@ -1,31 +1,31 @@
-package app;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-public class WebSeleniumTest {
-
-    WebDriver driver;
-
-    @Before
-    public void setUp () {
-//        System.setProperty("webdriver.chrome.driver", "WebDrivers/chromedriver");
-        driver = new ChromeDriver();
-    }
-
-    @Test
-    public void testWalpappersPage() throws Exception {
-
-        driver.navigate().to("http://localhost:8080/list.html");
-
-        (new WebDriverWait(driver, 10)).until((ExpectedCondition<Boolean>) (WebDriver d) -> d.getPageSource().contains("EMail")
-                && d.getPageSource().contains("Name")
-                && d.getPageSource().contains("Age")
-                && d.getPageSource().contains("Password"));
-        driver.close();
-    }
-}
+//package app;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//public class WebSeleniumTest {
+//
+//    WebDriver driver;
+//
+//    @Before
+//    public void setUp () {
+////        System.setProperty("webdriver.chrome.driver", "WebDrivers/chromedriver");
+//        driver = new ChromeDriver();
+//    }
+//
+//    @Test
+//    public void testWalpappersPage() throws Exception {
+//
+//        driver.navigate().to("http://localhost:8080/list.html");
+//
+//        (new WebDriverWait(driver, 10)).until((ExpectedCondition<Boolean>) (WebDriver d) -> d.getPageSource().contains("EMail")
+//                && d.getPageSource().contains("Name")
+//                && d.getPageSource().contains("Age")
+//                && d.getPageSource().contains("Password"));
+//        driver.close();
+//    }
+//}
